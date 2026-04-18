8e123ba8a82349652f94db8c1c9bd4ac8988cf81
Ade <adekunleadepoju916@gmail.com>
2026-04-18 09:50:33 +0100

    fix email port

--- rosee_backend/bibi_backend/settings.py
+++ rosee_backend/bibi_backend/settings.py
@@ -29,9 +29,9 @@ DATABASES = {'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'d
 # Production SMTP — uncomment and fill in:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'adekunleadepoju916@gmail.com'
 EMAIL_HOST_PASSWORD = 'kmnmoarpjefztlby'
 STUDIO_EMAIL = 'adekunleadepoju916@gmail.com'
